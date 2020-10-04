7c0b847601af928f7f8841251a336605c6f8e0b5
David Boland <dboland77@gmail.com>
2020-10-04 08:43:40 +0100

    working assignment

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import IngredientList from "./IngredientList";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeIngredientHandler = (id) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== id)
+    );
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
